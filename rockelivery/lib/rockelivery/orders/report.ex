d42b7f81cb526f83406baac2e65c1f4080053a8e
Giovanni Hessel <giovanni.hessel@fatec.sp.gov.br>
2021-06-02 16:33:12 -0300

    Added the timeout option to the report transaction to never expire

--- rockelivery/lib/rockelivery/orders/report.ex
+++ rockelivery/lib/rockelivery/orders/report.ex
@@ -10,13 +10,16 @@ defmodule Rockelivery.Orders.Report do
     query = from order in Order, order_by: order.user_id
 
     {:ok, order_list} =
-      Repo.transaction(fn ->
-        query
-        |> Repo.stream(max_rows: @default_block_size)
-        |> Stream.chunk_every(@default_block_size)
-        |> Stream.flat_map(fn chunk -> Repo.preload(chunk, :items) end)
-        |> Enum.map(&order_string/1)
-      end)
+      Repo.transaction(
+        fn ->
+          query
+          |> Repo.stream(max_rows: @default_block_size)
+          |> Stream.chunk_every(@default_block_size)
+          |> Stream.flat_map(fn chunk -> Repo.preload(chunk, :items) end)
+          |> Enum.map(&order_string/1)
+        end,
+        timeout: :infinity
+      )
 
     File.write(filename, order_list)
   end
